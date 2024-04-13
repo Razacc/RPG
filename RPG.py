92f8a5b9d08fbbb1d4a99d3a57f63f146282e0d4
Razacc <165668642+Razacc@users.noreply.github.com>
2024-04-13 23:11:32 +0200

    Update RPG.py

--- RPG.py
+++ RPG.py
@@ -6,6 +6,14 @@ class Personnage:
         self.points_de_vie = 10
         self.points_d_attaque = 5
         self.points_de_defense = 5
+
+    def increment_niveau(self):
+        self.niveau += 1
+        self.points_d_attaque += 2
+        self.points_de_defense += 2
+
+    def reduire_points_de_vie(self, points):
+        self.points_de_vie -= points
       
 # Création des personnages
 joueur1 = Personnage("Joueur 1", "Guerrier")
